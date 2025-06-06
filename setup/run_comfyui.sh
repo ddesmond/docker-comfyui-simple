98c6f366bcfc7a682ae5548e99026d426084c44c
Aleks <crni.aki@gmail.com>
2025-06-06 05:42:51 +0200

    updates

--- setup/run_comfyui.sh
+++ setup/run_comfyui.sh
@@ -2,4 +2,4 @@
 echo "_____ RUN COMFYUI _____"
 export PIP_ROOT_USER_ACTION=ignore
 
-python main.py --listen 0.0.0.0 --port 7860 --output-directory /outputs/ --disable-xformers --preview-method auto --multi-user
+python main.py --listen 0.0.0.0 --port 8188 --output-directory /outputs/ --disable-xformers --preview-method auto --multi-user
